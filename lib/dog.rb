ed543c89d9b1efaf2e1fe7ab4485cebfefcea128
brandon corey <bcorey85@gmail.com>
2019-04-11 06:32:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,21 +1,4 @@
 # Your code goes here!
-class Dog 
-  def initialize(name)
-    @name = name
-  end
-  
-  def name=(name)
-    @name = name
-  end
-  
-  def name
-    @name
-  end
-  
-  def bark
-    puts "woof!"
-  end
-end
 
 
 class Dog
